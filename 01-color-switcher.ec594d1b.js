df75ca27a6115583135c2144f3685bd5502288a6
martadelka <128497237+martadelka@users.noreply.github.com>
2023-09-18 21:30:54 +0000

    Deploying to gh-pages from @ martadelka/goit-js-hw-09@ea2e22e1a7957039db6d7c17dda0fc83ebbcb420 🚀

---
+++ 01-color-switcher.ec594d1b.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),o=document.querySelector("body"),r=0;function n(){o.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}t.addEventListener("click",(function(){r=setInterval(n,1e3),t.toggleAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(r),t.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.ec594d1b.js.map
